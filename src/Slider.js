e665781a53d73c7ebbb445e054caa9ee728d792a
everysum1 <israeldmatos@gmail.com>
2018-01-27 18:51:33 -0800

    Add basic Slider component w/ render func

---
+++ src/Slider.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Slider.css'
+
+class Slider extends Component {
+
+  render() {
+    return (
+      <div className="slider">
+
+      </div>
+    );
+  }
+}
+
+export default Slider;
