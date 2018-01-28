883bb00e593e42c6a32b270449dc77bfd8a95513
everysum1 <israeldmatos@gmail.com>
2018-01-27 18:52:10 -0800

    Add slider to App render func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Hero from './Hero';
+import Slider from './Slider';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Hero />
+        <Slider />
       </div>
     )
   }
