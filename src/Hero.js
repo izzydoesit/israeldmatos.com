814b958a0857bb4dd3f3cc75bf2e5fd248c99887
everysum1 <israeldmatos@gmail.com>
2018-01-27 17:09:41 -0800

    Add Hero class w/ basic render func

---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+
+  render() {
+    return(
+      <div className="hero">
+        Israel D. Matos
+      </div>
+    )
+  }
+}
+
+export default Hero;
