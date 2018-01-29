fe128685a08d5dcaa6981954cba445931fa06f2a
everysum1 <israeldmatos@gmail.com>
2018-01-28 17:02:03 -0800

    Delete unneeded event methods

--- src/RightArrow.js
+++ src/RightArrow.js
@@ -3,6 +3,20 @@ import 'font-awesome/css/font-awesome.css'
 import './RightArrow.css';
 
 export default class RightArrow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+  }
+
+  mouseOver() {
+    this.setState({hover: true});
+  }
+
+  mouseOut() {
+    this.setState({hover: false});
+  }
 
   render() {
     return (
