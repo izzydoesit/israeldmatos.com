d71b735d5bb609194b4a502f602c99ad6929c204
everysum1 <israeldmatos@gmail.com>
2018-02-09 13:52:25 -0800

    Add title & blog links to Blog component

--- src/Blog.js
+++ src/Blog.js
@@ -10,7 +10,41 @@ export default class Blog extends Component {
   render() {
     return (
       <ScrollableAnchor id={'blog'}>
+          <div className="header animated slide-in-left">Blog</div>
+          <div className="header-bar animated slide-in-left"></div>
 
+          <a href="">
+            <div className="animated fade-in fade">
+              <div className="thumb"></div>
+              <div className="label bold"></div>
+              <div className="bar"></div>
+              <div className="text"></div>
+            </div>                  
+          </a>
+          <a href="">
+            <div className="animated fade-in fade">
+              <div className="thumb"></div>
+              <div className="label bold"></div>
+              <div className="bar"></div>
+              <div className="text"></div>
+            </div>          
+          </a>
+          <a href="">
+            <div className="animated fade-in fade">
+              <div className="thumb"></div>
+              <div className="label bold"></div>
+              <div className="bar"></div>
+              <div className="text"></div>
+            </div>          
+          </a>
+          <a href="">
+            <div className="animated fade-in fade">
+              <div className="thumb"></div>
+              <div className="label bold"></div>
+              <div className="bar"></div>
+              <div className="text"></div>
+            </div>          
+          </a>
       </ScrollableAnchor>
     )
   }
