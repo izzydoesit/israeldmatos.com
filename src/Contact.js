860ec3d2b02a11a9e7f14ede5f5b298f96986f26
everysum1 <israeldmatos@gmail.com>
2018-02-09 13:52:40 -0800

    Add title & form to Contact component

--- src/Contact.js
+++ src/Contact.js
@@ -10,7 +10,23 @@ export default class Contact extends Component {
   render() {
     return (
       <ScrollableAnchor id={'contact'}>
+        <svg></svg>
+        <div className="container flex">
+          <div className="header animated slide-in-right">Contact</div>
+          <div className="header-bar animated slide-in-right"></div>
+          <div className="highlihght animated slide-in-right">Have a question or want to work together?</div>
 
+          <form className="animated pop-in">
+            <input placeholder="Name" type="text" name="name">
+            <input placeholder="Email" type="email" name="email">
+            <textarea placeholder="Your message" type="text" name="message"></textarea>
+
+            <div id="success">
+
+            </div>
+            <input className="button" type="submit" value="SUBMIT">
+          </form>
+        </div>
       </ScrollableAnchor>
     )
   }
