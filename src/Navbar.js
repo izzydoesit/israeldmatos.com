7729d4925a0e8d32dd0504a0bb15d38ae7f8b5f9
izzydoesit <israeldmatos@gmail.com>
2018-02-16 23:49:45 -0800

    Add link-wrap div around page links in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,18 +4,18 @@ import './Navbar.css';
 export default class Navbar extends Component {
   constructor(props) {
     super(props);
-
-
   }
 
   render() {
     return (
-      <nav className="flex" >
-        <a className="page-link active" href="#home">Home</a>
-        <a className="page-link" href="#about">About</a>
-        <a className="page-link" href="#projects">Projects</a>
-        <a className="page-link" href="#blog">Blog</a>
-        <a className="page-link" href="#contact">Contact</a>
+      <nav className="flex nav" >
+        <div className="link-wrap">
+          <a className="page-link active" href="#home">Home</a>
+          <a className="page-link" href="#about">About</a>
+          <a className="page-link" href="#projects">Projects</a>
+          <a className="page-link" href="#blog">Blog</a>
+          <a className="page-link" href="#contact">Contact</a>
+        </div>
       </nav>
     )
   }
