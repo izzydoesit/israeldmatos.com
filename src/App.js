9a27efd8c4d164f73f5e100c6f3f205097ce19e3
izzydoesit <israeldmatos@gmail.com>
2018-02-18 15:49:49 -0800

    Add footer styles & render in App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import Headroom from 'react-headroom';
-import ScrollableAnchor from 'react-scrollable-anchor/lib/ScrollableAnchor';
 import Home from './Home';
 import Navbar from './Navbar';
 import About from './About';
 import Projects from './Projects';
 import Blog from './Blog';
 import Contact from './Contact';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-          <div className="App">
+      <div className="App">
 
-            <Headroom
-              onPin={() => console.log('pinned')}
-              onUnpin={() => console.log('unpinned')}
-            >
-              <Navbar/>
-            </Headroom>
+        <Headroom
+          onPin={() => console.log('pinned')}
+          onUnpin={() => console.log('unpinned')}
+        >
+          <Navbar/>
+        </Headroom>
 
-            <Home />
-            <About />
-            <Projects />
-            {/* <Blog /> */}
-            {/* <Contact /> */}
-
-          </div>
+        <Home />
+        <About />
+        <Projects />
+        {/* <Blog /> */}
+        <Contact />
+        <Footer />
+      </div>
     )
   }
 }
