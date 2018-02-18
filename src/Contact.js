50cd383e62ad1372686fbf9b2123fca9024589dc
izzydoesit <israeldmatos@gmail.com>
2018-02-18 11:01:40 -0800

    Add contact form component w/ handler functions

--- src/Contact.js
+++ src/Contact.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import ScrollableAnchor from 'react-scrollable-anchor/lib/ScrollableAnchor';
+import ContactForm from './ContactForm';
 
 export default class Contact extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
       <section>
         <ScrollableAnchor id={'contact'}>
-          <svg></svg>
+      
           <div className="container flex">
             <div className="header animated slide-in-right">Contact</div>
             <div className="header-bar animated slide-in-right"></div>
-            <div className="highlihght animated slide-in-right">Have a question or want to work together?</div>
+            <div className="subtitle animated slide-in-right">Have a question or want to work together?</div>
 
-            <form className="animated pop-in">
-              {/* <input placeholder="Name" type="text" name="name">
-              <input placeholder="Email" type="email" name="email"> */}
-              <textarea placeholder="Your message" type="text" name="message"></textarea>
-
-              <div id="success">
-                <div>Your message has been submitted successfully</div>
-              </div>
-              <input className="button" type="submit" value="SUBMIT" />
-            </form>
+            <ContactForm />
           </div>
         </ScrollableAnchor>
       </section>
