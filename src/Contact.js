72e554c18b9849c1232a7250f341260fdb1022dc
izzydoesit <israeldmatos@gmail.com>
2018-02-18 15:51:21 -0800

    Add Contact section animation & styling

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import ScrollableAnchor from 'react-scrollable-anchor/lib/ScrollableAnchor';
+import ScrollAnimation from 'react-animate-on-scroll';
 import ContactForm from './ContactForm';
 
 export default class Contact extends Component {
@@ -11,11 +12,20 @@ export default class Contact extends Component {
         <ScrollableAnchor id={'contact'}>
       
           <div className="container flex">
-            <div className="header animated slide-in-right">Contact</div>
-            <div className="header-bar animated slide-in-right"></div>
-            <div className="subtitle animated slide-in-right">Have a question or want to work together?</div>
 
-            <ContactForm />
+            <ScrollAnimation animateIn="bounceInRight">
+              <div className="header">Contact</div>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="bounceInRight" delay={500}>
+              <div className="header-bar"></div>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="bounceInLeft" delay={900}>
+              <div className="subtitle">Have a question or want to work together?</div>
+            </ScrollAnimation>
+
+
+              <ContactForm />
+
           </div>
         </ScrollableAnchor>
       </section>
