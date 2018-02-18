06f45bfe379f92b5859b881224153d123811b70d
izzydoesit <israeldmatos@gmail.com>
2018-02-18 15:52:13 -0800

    Add scroll animation & styling for ContactForm comp

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Close from 'react-icons/lib/fa/close';
+import ScrollAnimation from 'react-animate-on-scroll';
 import emailjs from 'emailjs-com';
 import './ContactForm.css';
 
@@ -32,7 +33,7 @@ export default class ContactForm extends Component {
     this.setState({opacity: 1})
     console.log('SUBMITTED!!!', event.target.value);
     alert('A name was submitted: ' + this.state.value);
-    
+
     emailjs.init(userId);
     /* emailjs.send(serviceId, templateId, {
       reply_to: "",
@@ -55,44 +56,50 @@ export default class ContactForm extends Component {
     const { name, email, message } = this.state;
 
     return (
-      <form className="flex contact-form">
-        <div className="flex input-wrap">
-          <input 
-            className="flex name-input"
-            type="text" 
-             placeholder="Name" 
-             name="name" 
-             value={name} 
-             onChange={this.onChange} 
-          />
-          <input 
-            className="flex email-input"
-            type="text" 
-            placeholder="Your email"
-             name="email" 
-             value={email} 
-             onChange={this.onChange} 
-          />
-          <textarea 
-            className="flex message-input"
-            type="text" 
-            placeholder="Your message..."
-            name="message"
-             value={message} 
-             onChange={this.onChange} 
-          />
-          <div id="success" className="expand" styles={{opacity: this.state.opacity}}>
-            <div> 
-              "Your message was sent successfully.  Thanks!"
-              <span id="close" class="" onClick={this.onClose}>
-                <Close size={20} className="close-icon"/>
-              </span>
+      <div>
+        <ScrollAnimation animateIn="tada" delay={800}>
+            
+          <form className="flex contact-form">
+            <div className="flex input-wrap">
+              <input 
+                className="flex name-input"
+                type="text" 
+                placeholder="Name" 
+                name="name" 
+                value={name} 
+                onChange={this.onChange} 
+              />
+              <input 
+                className="flex email-input"
+                type="text" 
+                placeholder="Your email"
+                name="email" 
+                value={email} 
+                onChange={this.onChange} 
+              />
+              <textarea 
+                className="flex message-input"
+                type="text" 
+                placeholder="Your message..."
+                name="message"
+                value={message} 
+                onChange={this.onChange} 
+              />
+              <div id="success" className="expand" styles={{opacity: this.state.opacity}}>
+                <div> 
+                  "Your message was sent successfully.  Thanks!"
+                  <span id="close" class="" onClick={this.onClose}>
+                    <Close size={20} className="close-icon"/>
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <button className="contact-btn" type="submit" value="Submit">Submit</button>
-      </form>
+            <button className="contact-btn" type="submit" value="Submit">Submit</button>
+          </form>
+
+        </ScrollAnimation>
+      </div>
     )
   }
 }
