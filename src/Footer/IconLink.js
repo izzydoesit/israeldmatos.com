4dfcd0cbba87a83c79ffd3ede007903b07a4b544
izzydoesit <israeldmatos@gmail.com>
2018-02-24 20:35:25 -0800

    Improve footer mobile layout, move files to separate folder

---
+++ src/Footer/IconLink.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+export default class IconLink extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const Icon = this.props.icon;
+
+    return (
+      <a
+        className="flex social-link"
+        href={this.props.url}
+      >
+        <Icon size={40} className="icon" />
+      </a>
+    )
+  }
+}
