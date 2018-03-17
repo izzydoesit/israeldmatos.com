15c0f7a6e045f00d65a290013cb55186dde79496
izzydoesit <israeldmatos@gmail.com>
2018-03-17 05:18:48 -0700

    Add gmail password env var, refactor config object export

--- mailer/config/index.js
+++ mailer/config/index.js
@@ -3,11 +3,13 @@ const refreshToken = process.env.REACT_APP_GMAIL_REFRESH_TOKEN
 const accessToken = process.env.REACT_APP_GMAIL_ACCESS_TOKEN
 const clientId = process.env.REACT_APP_GMAIL_CLIENT_ID
 const clientSecret = process.env.REACT_APP_GMAIL_CLIENT_SECRET
+const password = process.env.REACT_APP_GMAIL_PASSWORD
 
 module.exports = {
-  user,
-  clientId,
-  clientSecret,
-  refreshToken,
-  accessToken
+  user: user,
+  pass: password,
+  clientId: clientId,
+  clientSecret: clientSecret,
+  refreshToken: refreshToken,
+  accessToken: accessToken
 }
