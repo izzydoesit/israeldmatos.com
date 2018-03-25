2eb1fb7a484855b8b188b424f44685444e3aa177
izzydoesit <israeldmatos@gmail.com>
2018-03-24 21:43:03 -0700

    Add ModalLauncher w/ button & show state, w/ stylesheet

---
+++ src/Modal/ModalLauncher.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Modal from './Modal';
+import './ModalLauncher.css';
+
+export default class ModalLauncher extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  handleToggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+  
+  render() {
+    const { showModal } = this.state;
+
+    return (
+      <div>
+        <button
+          type="button"
+          className="modalButton"
+          onClick={() => this.handleToggleModal()}
+        >
+          Learn More
+        </button>
+
+        {showModal &&
+          <Modal onCloseRequest={() => this.handleToggleModal()}>
+            <img src="https://placeimg.com/900/650/nature" alt="Nature" />
+          </Modal>}
+      </div>
+    );
+  }
+}
