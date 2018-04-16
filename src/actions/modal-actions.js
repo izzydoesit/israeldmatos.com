4bd16de2a023b30ba5677bf4538eca4610d07f7e
izzydoesit <israeldmatos@gmail.com>
2018-04-15 23:03:50 -0700

    Add actions for card & modal

---
+++ src/actions/modal-actions.js
@@ -0,0 +1,11 @@
+import { UPDATE_MODAL, TOGGLE_MODAL } from '../constants';
+
+export const updateModal = (currentProject) => ({
+  type: UPDATE_MODAL,
+  currentProject
+});
+
+export const toggleModal = (modalState) => ({
+  type: TOGGLE_MODAL,
+  modalOpen: modalState,
+})
