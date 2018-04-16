1b860a28ace7130fc9cea137563ee241827c4df0
izzydoesit <israeldmatos@gmail.com>
2018-04-15 23:09:58 -0700

    Add Provider wrapper w/ Redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './Containers/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { configureStore } from './Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>, 
+  document.getElementById('root')), registerServiceWorker();
