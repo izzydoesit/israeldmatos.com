64d128cef5a53c988324a6a7078a02632d9df072
izzydoesit <israeldmatos@gmail.com>
2018-06-16 17:20:20 -0700

    Update navbar w/ new background

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -33,7 +33,7 @@ handleScroll(event) {
     return (
       <nav className="flex nav" >
         <div className="link-wrap">
-          <a className="page-link active" href="#home">Home</a>
+          <a className="page-link active" href="#landing">Home</a>
           <a className="page-link" href="#about">About</a>
           <a className="page-link" href="#projects">Projects</a>
           {/* <a className="page-link" href="#blog">Blog</a>*/}
