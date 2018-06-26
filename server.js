9e395d6ba71e73b25ddf039ddefe5925976160ec
izzydoesit <israeldmatos@gmail.com>
2018-06-26 03:52:18 -0700

    add slash to server file

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (!dev) {
   app.use(express.static(path.join(__dirname, 'build')));
   
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', '/index.html'));
   })
 }
 
