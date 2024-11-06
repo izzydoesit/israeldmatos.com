16baacc00bf4faaf61e73c41e41a384e1d7a5325
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2024-11-05 22:39:41 -0500

    Add basic hero component & styles

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { hero, name, content } from "./hero.module.css";
+import { hero, name, content, wrapper } from "./hero.module.css";
 import { Snowfall } from "react-snowfall";
 
 const Hero: React.FC = () => {
   return (
     <section className={hero}>
-      <div>
+      <div className={wrapper}>
         <div className={content}>
           Hello, I am <span className={name}>Israel D. Matos</span> I am a
           software engineer and this is my HERO section.
