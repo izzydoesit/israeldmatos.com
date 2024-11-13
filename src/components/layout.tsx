1ca44a2ae20b3ca7f8ceef23e060845f980e4813
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2024-11-13 12:23:36 -0500

    delete header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -51,24 +51,8 @@ const icons = [
 const Layout: React.FC<LayoutProps> = ({ pageTitle, children }) => {
   return (
     <div className={container}>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
+      <main>{children}</main>
+
       <footer className={footer}>
         <a href="#home" className={chevronLinkWrapper}>
           <FaChevronUp className={chevron} />
