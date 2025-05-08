f015217239b6c7e61f7947cf58498f26e9e9914b
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-08 06:12:20 -0400

    Add contact heading w/ animation & form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
-import { contact } from "../styles/contact.module.css";
+import { motion } from "framer-motion";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
-  return (
-    <section className={contact}>
-      <div>
-        <p>This is my contact section</p>
-      </div>
-    </section>
-  );
+	return (
+		<section
+			id="contact"
+			className="flex flex-col max-w-xl mx-auto px-6 py-16 border-purple-500"
+		>
+			{/* Title */}
+			<motion.div
+				className="text-center mb-16"
+				initial={{ opacity: 0, y: -40 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.6 }}
+				viewport={{ once: true }}
+			>
+				<h2 className="text-4xl text-white font-bold mb-8 text-center">
+					Contact Me
+				</h2>
+			</motion.div>
+
+			{/* Form */}
+			<div>
+				<ContactForm />
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
