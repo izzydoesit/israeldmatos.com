2dd0c4926af7770ca1b4188b4c5ed69598f578cc
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-08 01:22:47 -0400

    Add CTA button link to Hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,11 +4,14 @@ import { Snowfall } from "react-snowfall";
 
 const Hero: React.FC = () => {
   return (
-    <section className={hero}>
+    <section id="home" className={hero}>
       <div className={wrapper}>
         <div className={content}>
           <h1>Hello, I am <span className={name}>Israel D. Matos 👋</span></h1>
           <p>Software Engineer. Polyglot. Builder of cool stuff.</p>
+          <a href="#about" className="mt-4">
+            <button className="bg-highlight-color hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">Learn More...</button>
+          </a>
         </div>
         <Snowfall snowflakeCount={500} />
       </div>
