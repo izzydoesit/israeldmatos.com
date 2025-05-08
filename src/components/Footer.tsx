1518801b6f895bc01d98fb0c89d1f27452302f48
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-07 22:12:16 -0400

    Move Footer elements from Layout to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,38 +4,54 @@ import LinkedIn from 'react-icons/lib/fa/linkedin';
 import Facebook from 'react-icons/lib/fa/facebook';
 import Instagram from 'react-icons/lib/fa/instagram';
 import Github from 'react-icons/lib/fa/github';
+import { FaChevronUp } from "@react-icons/all-files/fa/FaChevronUp";
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
+import { FaFacebook } from "@react-icons/all-files/fa/FaFacebook";
+import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { footer } from "../styles/layout.module.css";
+import { chevronLinkWrapper, socialLink, socialLinkIcon, socialLinkRow, chevron, infoBox, footNote, highlight } from '../styles/layout.module.css';
 
 const icons = [
-    { src: LinkedIn, url: 'https://www.linkedin.com/in/israeldmatos' },
-    { src: Facebook, url: 'https://www.facebook.com/israel.matos.963' },
-    { src: Instagram, url: 'https://www.instagram.com/izzytrips/' },
-    { src: Github, url: 'https://www.github.com/izzydoesit' },
+    { src: FaLinkedin, url: 'https://www.linkedin.com/in/izzy-matos' },
+    { src: FaFacebook, url: 'https://www.facebook.com/israel.matos.963' },
+    { src: FaInstagram, url: 'https://www.instagram.com/izzydoesreels/' },
+    { src: FaGithub, url: 'https://www.github.com/izzydoesit' },
   ]
 
 const Footer: React.FC = () => {
     return (
-        <footer id="footer">
-          <a href="#home" className="chevron-link-wrapper">
-            <Chevron className="chevron" />
-          </a>
+        <footer id="footer" className={footer}>
+            <a href="#footer" className={chevronLinkWrapper}>
+                <FaChevronUp className={chevron} />
+            </a>
   
-          <div className="flex row icon-wrap">
-            {icons.map((icon, index) =>
-              <IconLink
-                key={index}
-                index={index}
-                icon={icon.src}
-                url={icon.url}
-              />
-            )}
-          </div>
-  
-          <div className="info-box">
-            <div className="foot-note">
-              ISRAEL D. MATOS
-              <span className="highlight">©2017</span>
+            <div className={socialLinkRow}>
+                {icons.map((icon, index) => {
+                    const Icon = icon.src;
+    
+                    return (
+                        <a
+                        key={index}
+                        className={socialLink}
+                        href={icon.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                            <Icon className={socialLinkIcon} size={40} />
+                        </a>
+                    );
+                })}
             </div>
-          </div>
+                
+            <div className={infoBox}>
+                <div className={footNote}>
+                    ISRAEL D. MATOS
+                    <span className={highlight}>©2025</span>
+                </div>
+            </div>
         </footer>
-      )
+    )
 }
+
+export default Footer;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import type { HeadFC, PageProps } from "gatsby";
+import Footer from "./Footer"
 import {
   container,
   heading,
@@ -18,70 +19,16 @@ import {
   highlight,
 } from "../styles/layout.module.css";
 
-import { FaChevronUp } from "@react-icons/all-files/fa/FaChevronUp";
-import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
-import { FaFacebook } from "@react-icons/all-files/fa/FaFacebook";
-import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram";
-import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
-
 type LayoutProps = {
   pageTitle: string;
   children: PageProps["children"];
 };
 
-const icons = [
-  {
-    src: FaLinkedin,
-    url: "https://www.linkedin.com/in/israeldmatos",
-  },
-  {
-    src: FaFacebook,
-    url: "https://www.facebook.com/israel.matos.963",
-  },
-  {
-    src: FaInstagram,
-    url: "https://www.instagram.com/izzydoesreels/",
-  },
-  {
-    src: FaGithub,
-    url: "https://www.github.com/izzydoesit",
-  },
-];
-
 const Layout: React.FC<LayoutProps> = ({ pageTitle, children }: LayoutProps) => {
   return (
     <div className={container}>
       <main>{children}</main>
-
-      <footer className={footer}>
-        <a href="#home" className={chevronLinkWrapper}>
-          <FaChevronUp className={chevron} />
-        </a>
-        <div className={socialLinkRow}>
-          {icons.map((icon, index) => {
-            const Icon = icon.src;
-
-            return (
-              <a
-                key={index}
-                className={socialLink}
-                href={icon.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon className={socialLinkIcon} size={40} />
-              </a>
-            );
-          })}
-        </div>
-
-        <div className="infoBox">
-          <div className={footNote}>
-            ISRAEL D. MATOS
-            <span className={highlight}>©2024</span>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
