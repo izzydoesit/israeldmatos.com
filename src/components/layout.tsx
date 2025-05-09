de49c4df38f04fd6ecd885e636a4c1266f76c08d
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-09 03:00:52 -0400

    Remove unused imports from Layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,22 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import type { HeadFC, PageProps } from "gatsby";
-import Footer from "./Footer"
-
+import type { PageProps } from "gatsby";
+import Footer from "./Footer";
 
 type LayoutProps = {
-  pageTitle: string;
-  children: PageProps["children"];
+	pageTitle: string;
+	children: PageProps["children"];
 };
 
-const Layout: React.FC<LayoutProps> = ({ pageTitle, children }: LayoutProps) => {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-      <main className="flex-grow px-4 py-8">{children}</main>
-      <Footer />
-    </div>
-  );
+const Layout: React.FC<LayoutProps> = ({
+	pageTitle,
+	children,
+}: LayoutProps) => {
+	return (
+		<div className="min-h-screen flex flex-col bg-gray-900 text-white">
+			<main className="flex-grow">{children}</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
