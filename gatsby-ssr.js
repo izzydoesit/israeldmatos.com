e4fbfab299774178cfee73bd699657601a8b2e1f
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-16 00:02:19 -0400

    Undo formspree Provider configs

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FormspreeProvider } from "@formspree/react";
-
-export const wrapRootElement = ({ element }) => (
-	<FormspreeProvider project={process.env.GATSBY_FORMSPREE_PROJECT_ID}>
-		{element}
-	</FormspreeProvider>
-);
